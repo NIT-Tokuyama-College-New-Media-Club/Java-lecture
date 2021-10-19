b4c7bd00090c74e5a74930c98c02687f422eea57
i18furuta <fn.nk.2002@gmail.com>
2021-10-19 09:22:49 +0900

    Fix 4

---
+++ 4_library/MathTest.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+
+public class MathTest {
+    public static void main(String[] args) {
+        double ans = Math.pow(3.0, 2.0);
+
+        System.out.println(ans);
+    }
+}
+
